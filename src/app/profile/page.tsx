1b6f70343f47729b7dfeb8d3883053c121f95481
KineshLohar <kineshmalviya@gmail.com>
2024-10-07 14:37:42 +0530

    middleware jjs

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,34 @@
+"use client";
 
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
+export default function Profile() {
+  const router = useRouter();
 
-export default function Profile(){
-    return(
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-2xl font-bold">Profile</h1>
-            <p>Welcome to your profile page</p>
-        </div>
-    )
+  const logout = async () => {
+    try {
+      const response = await axios.get("/api/users/logout");
+      toast.success("User logged out");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error("Error logging out");
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Profile</h1>
+      <p>Welcome to your profile page</p>
+      <hr />
+      <button
+        onClick={logout}
+        className="px-4 py-2 bg-blue-500 text-white rounded-lg mt-4"
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
