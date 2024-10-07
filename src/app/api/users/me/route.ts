cdde3f6b6301bac00373cc4948f5d14a396e849f
Kinesh Lohar <kineshmalviya@gmail.com>
2024-10-07 15:19:17 +0530

    working on get User route

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,38 @@
+import User from "@/models/userModel";
+import connectDB from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+import getDataFromToken from "@/helpers/getDataFromToken";
+
+connectDB()
+
+export default async function GET(request : NextRequest){
+    try {
+        const userId = await getDataFromToken(request);
+        const user = await User.findOne({
+            _id : userId
+        }).select("-password")
+
+        if(!user){
+            return NextResponse.json({
+                error : "User Does Not Exist!"
+            },{
+                status : 400
+            })
+        }
+
+        return NextResponse.json({
+            message : "User data fetched successfully!",
+            data : user
+        },{
+            status : 200
+        })
+
+    } catch (error : any) {
+        return NextResponse.json({
+            error : error.message
+        },
+        {
+            status : 400
+        })
+    }
+}
